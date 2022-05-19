65e629e15a0cd5a615fb85996a5d4ec1bebebd2a
Jaya-SI <60172081+Jaya-SI@users.noreply.github.com>
2022-05-19 21:04:09 +0800

    belajar test

---
+++ config/contoh.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+        "name" => [
+            "first" => "Jaya",
+            "Last" => "Saputra"
+        ]
+        "email" => "info.saput@gmail.com",
+        "instagram" => "@jaya.sp"
+    ];
+
+?>
